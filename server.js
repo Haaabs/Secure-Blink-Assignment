dc3e90bfd260e5837086e13cb8f94e3a7143e2a3
Haaabs <akotti99@gmail.com>
2022-08-30 14:44:30 +0530

    initial commit

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 const { appendFile } = require('fs')
-
-appendFile.use('/', serveStatic(path.join(__dirname, '/dist')))
+const app = express()
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 const port = 8080
 app.listen(port)
